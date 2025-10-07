36b5c3241a4516e2978722fa86ab9ade8f9ee7c5
Qingyue <2923717811@qq.com>
2025-10-07 22:30:33 +0800

    修复协程作用域被销毁导致cookie无法正常保存的bug

--- spider_app/src/main/java/com/example/spider_app/MainActivity.kt
+++ spider_app/src/main/java/com/example/spider_app/MainActivity.kt
@@ -8,11 +8,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.dcelysia.csust_spider.core.Resource
+import com.dcelysia.csust_spider.core.RetrofitUtils
 import com.dcelysia.csust_spider.education.data.remote.EducationHelper
 import com.dcelysia.csust_spider.education.data.remote.services.AuthService
 import com.dcelysia.csust_spider.education.data.remote.services.EduCourseService
 import com.dcelysia.csust_spider.mooc.data.remote.repository.MoocRepository
 import com.example.csustdataget.CampusCard.CampusCardHelper
+import com.example.spider_app.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.filter
@@ -21,14 +23,17 @@ import kotlinx.coroutines.launch
 import java.net.Authenticator
 
 class MainActivity : AppCompatActivity() {
+    private val binding : ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         val loginButton = findViewById<Button>(R.id.loginButton)
         loginButton.setOnClickListener {
             CoroutineScope(Dispatchers.IO).launch {
+                RetrofitUtils.ClearClient("moocClient")
+                RetrofitUtils.ClearClient("EducationClient")
                 try {
                     val loginResult = MoocRepository.instance.login("202408130230","@Wsl20060606")
                     .filter { it !is Resource.Loading }
@@ -46,6 +51,12 @@ class MainActivity : AppCompatActivity() {
                     Log.d(TAG, e.toString())
                 }
 
+            }
+        }
+        binding.course.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                val course = EducationHelper.getCourseScheduleByTerm("","2025-2026-1")
+                Log.d(TAG,"course:${course}")
             }
         }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
