b7be73f82b6125b8dcd049a96284bb4ff03d2f95
timeisthe <202408130230@stu.csust.edu.cn>
2025-10-11 17:14:25 +0800

    持久化账号密码

--- csust_spider/src/main/java/com/dcelysia/csust_spider/education/data/remote/EducationData.kt
+++ csust_spider/src/main/java/com/dcelysia/csust_spider/education/data/remote/EducationData.kt
@@ -1,8 +1,24 @@
 package com.dcelysia.csust_spider.education.data.remote
 
+import com.tencent.mmkv.MMKV
+
 object EducationData {
-    var studentId: String = ""
-    var studentPassword : String = ""
+    private val mmkv by lazy { MMKV.mmkvWithID("education_cache") }
+    private const val KEY_STUDENT_ID = "student_id"
+    private const val KEY_PASSWORD = "student_password"
+
+    var studentId: String
+        get() = mmkv.getString(KEY_STUDENT_ID, "") ?: ""
+        set(value) {
+            mmkv.putString(KEY_STUDENT_ID, value)
+        }
+
+    var studentPassword: String
+        get() = mmkv.getString(KEY_PASSWORD, "") ?: ""
+        set(value) {
+            mmkv.putString(KEY_PASSWORD, value)
+        }
+
     fun clear() {
         studentId = ""
         studentPassword = ""
