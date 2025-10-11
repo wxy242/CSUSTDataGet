10aa6b0a299e83823276296698efa9c2f0c70cd5
timeisthe <202408130230@stu.csust.edu.cn>
2025-10-11 16:28:10 +0800

    1.分离教务的登录和获取数据的客户端 2.添加用于教务数据客户端的拦截器

---
+++ csust_spider/src/main/java/com/dcelysia/csust_spider/education/data/remote/EducationData.kt
@@ -0,0 +1,10 @@
+package com.dcelysia.csust_spider.education.data.remote
+
+object EducationData {
+    var studentId: String = ""
+    var studentPassword : String = ""
+    fun clear() {
+        studentId = ""
+        studentPassword = ""
+    }
+}
